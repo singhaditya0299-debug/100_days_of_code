362f42320ce00d8fc987edf2c7adc4ae40f3d929
Aditya Singh <singhaditya0299@gmail.com>
2026-08-17 08:26:49 +0530

    day01 program 01

---
+++ 01Day_01_program/01_program.c
@@ -0,0 +1,29 @@
+//  Q1: Write a program to input two numbers and display their sum.
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+
+#include<stdio.h>
+int main(){
+    int a,b,c=0;
+    printf("Enter your first number\n");
+    scanf("%d",&a);
+    printf("Enter your second number\n");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The sum is %d",c);
+    return 0;
+
+
+}
