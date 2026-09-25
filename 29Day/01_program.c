99e8db887cbb7c81181b2a1af179f9755e288b8e
Aditya Singh <singhaditya0299@gmail.com>
2026-09-25 20:35:05 +0530

    array

---
+++ 29Day/01_program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,a[n],c=0;
+    printf("Enter a number:\n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+    printf("Enter a value in %d position :\n",i+1);
+    scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++){
+        c+=a[i];
+    }
+    printf("The sum is %d",c);
+    return 0;
+}
---
+++ 29Day/02_program.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,a[n],h=0;
+    printf("Enter a number:\n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+    printf("Enter a value in %d position :\n",i+1);
+    scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>h){
+            h=a[i];
+        }
+    }
+    printf("The highest value is: %d",h);
+    return 0;
+}
